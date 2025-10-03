c5282fa0533bf94552e820dd1ca139d310ac1481
RjSup <19141558@brookes.ac.uk>
2025-10-03 16:25:07 +0100

    Initial Commit

---
+++ ChallengeApp/ChallengeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengeAppApp.swift
+//  ChallengeApp
+//
+//  Created by Ryan on 03/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct ChallengeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
